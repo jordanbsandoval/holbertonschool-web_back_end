2a4d61c3b3ffc437418c4750f6be8fcdc4261e10
jordanbsandoval <1608@holbertonschool.com>
2022-02-16 00:13:51 -0400

    index-app

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -20,6 +20,13 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")

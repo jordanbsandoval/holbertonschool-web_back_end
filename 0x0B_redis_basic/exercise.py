07967cc9410f98fa9f4f06eb683111ee70e7a8e5
jordanbsandoval <1608@holbertonschool.com>
2022-03-15 20:22:55 -0400

    Storing lists

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -7,17 +7,29 @@ import redis
 import uuid
 import sys
 from functools import wraps
-
-
 def count_calls(method: Callable) -> Callable:
     """ Calls counter decorator """
     key = method.__qualname__
-
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         """ Method wrapper to incr count """
         self._redis.incr(key)
-        return(self, args, kwargs)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+def call_history(method: Callable) -> Callable:
+    """ Calls call history decorator """
+    key = method.__qualname__
+    input_key = key + ":inputs"
+    output_key = key + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Method wrapper to incr count """
+        self._redis.rpush(input_key, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(data))
+        return data
     return wrapper
 
 
@@ -28,19 +40,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Constructor """
-        random_id = str(uuid.uuid4())
-        self._redis.set(random_id, data)
-        return random_id
-    def get(self, key: str,
-            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
-        """ Get originaltype data """
-        return fn(self._redis.get(key)) if fn else self._redis.get(key)
-    def get_str(self, value: bytes) -> str:
-        """ Converts bytes to string """
-        return value.decode("utf-8")
-    def get_int(self, value: bytes) -> str:
-        """ Converts bytes to integer """
-        return int.from_bytes(value, sys.byteorder)

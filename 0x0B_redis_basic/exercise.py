baa7b42a0151e5e5426cd1175dbe7257a792c059
jordanbsandoval <1608@holbertonschool.com>
2022-03-15 20:10:21 -0400

    Reading from Redis and recovering original type3

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -31,15 +31,3 @@ class Cache:
         val = self._redis.get(key)
         return val if not fn else fn(val)
 
-
-cache = Cache()
-
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value

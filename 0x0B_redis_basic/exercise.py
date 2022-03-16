69dbfb3458095549197a22fd33d21596a906c3b5
jordanbsandoval <1608@holbertonschool.com>
2022-03-15 20:07:29 -0400

    Reading from Redis and recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """ Writing strings to Redis  """
-from typing import Union
+from typing import Callable, Optional, Union
 import redis
 import uuid
-
 
 class Cache:
     """ Cache class """
@@ -11,10 +10,34 @@ class Cache:
         """ constructor """
         self._redis = redis.Redis()
         self._redis.flushdb()
-
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Generates a random key """
         id = str(uuid.uuid4())
         self._redis.set(id, data)
         return id
 
+    def get_str(self, key):
+        """  parametrize Cache.get with the correct conversion function """
+        return self.get(key, str)
+
+    def get_int(self, key):
+        """  parametrize Cache.get with the correct conversion function """
+        return self.get(key, int)
+
+    def get(self, key: str, fn: Optional[Callable]):
+        """ convert the data back to the desired format """
+        val = self._redis.get(key)
+        return val if not fn else fn(val)
+
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value

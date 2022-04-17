458bf491cba716dc4a7ad7886f51f8bc63ae4ffc
jordanbsandoval <1608@holbertonschool.com>
2022-04-17 19:37:00 -0400

    Using Process stdin

---
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (d) => {
+  if (d.toString().trim() !== '') {
+    process.stdout.write(`Your name is: ${d.toString()}`);
+  } else {
+    process.exit();
+  }
+});
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+  process.exit();
+});
